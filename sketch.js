6f6cb563e24e603c4e8dbee2a5524793e9e590b0
coronel07 <d51702027@alumnos.itr.edu.ar>
2022-08-26 11:26:59 -0300

    commit 3

--- sketch.js
+++ sketch.js
@@ -1,40 +1,91 @@
 let frames = 8;
 let ancho = 800;
-let largo = 900;
+let largo = 800;
 let posX;
 let posY;
+let plane;
+let ovni;
+let bullets;
+
+
+function preload(){
+	plane = new Sprite(400,650,10,10);
+	plane.addImg("./assets/plane.png");
+
+	ovni = new Sprite(400,50,10,10);
+	ovni.addImg("./assets/ovni.png");
+
+	bullets = new Group();
+	bullets.addImg("./assets/bullet.png");
+}
 
 function setup() {
 	frameRate(frames);
-	translate(width / 2, height / 2);
-	createCanvas(ancho, largo);
-
-	plane1 = createSprite(200, 200, 20, 20);
-	plane1.shapeColor = (124, 252, 0);
-
+	let canvas = createCanvas(ancho, largo);
+	//canvas.parent("canvas-content")
+	background(150);
+	
+	EvilOvni();
 }
 
 function draw() {
-	if (keyIsDown(LEFT_ARROW)) {
-		  posX -= 5;
-	}
-	  
-	if (keyIsDown(RIGHT_ARROW)) {
-		  posX += 5;
-	}
-	plane.velocity.x = posX;
-
-
 	clear();
-	background(220);
-	drawSprites();
+	strokeWeight(3);
+	noFill();
+	square(0,0,800);
+
+	
+	for (let s of allSprites) {
+		if (s.x < -MARGIN) s.x = width + MARGIN;
+		if (s.x > width + MARGIN) s.x = -MARGIN;
+		if (s.y < -MARGIN) s.y = height + MARGIN;
+		if (s.y > height + MARGIN) s.y = -MARGIN;
+	}
+
+	
+	/*if (kb.pressed('a')) {
+		plane.vel.x -= 15;
+	}else if (kb.pressed('d')) {
+		plane.vel.x += 15;
+	}else{
+		plane.vel.x = 0;
+	}*/
 }
 
-/*function keyPressed() {
-	if(keyCode === LEFT_ARROW != +20){
-		posX = -20;
-	}else if(keyCode === RIGHT_ARROW != -20){
-		posX = +20;
+function keyPressed(){
+	if(keyCode === LEFT_ARROW && keyIsDown){
+		plane.vel.x -= 18;
+		ovni.vel.x -= 30;
 	}
-	plane.velocity.x = posX;
-}*/
+	if(keyCode === RIGHT_ARROW && keyIsDown){
+		plane.vel.x += 18;
+		ovni.vel.x += 30;
+	}
+
+	if(plane.vel.x <= -18){
+		plane.vel.x = -18;
+	}
+
+	if(plane.vel.x >= 18){
+		plane.vel.x = 18;
+	}
+
+	if(ovni.vel.x <= -30){
+		ovni.vel.x = -30;
+	}
+
+	if(ovni.vel.x >= 30){
+		ovni.vel.x = 30;
+	}
+}
+
+function EvilOvni(){
+	
+}
+
+function mousePressed(){
+		bullet = new bullets.Sprite(plane.x, plane.y -50);
+		//bullet.direction = plane.direction;
+		bullet.vel.y = -30;
+		bullet.life = 30; 
+}
